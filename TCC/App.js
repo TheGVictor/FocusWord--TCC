141a5493e87905cc17109d2c51065658707935a0
renan <renanss2005@gmail.com>
2023-06-15 08:21:50 -0300

    commit inicial

--- TCC/App.js
+++ TCC/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Shape em desenvolvimento!!!</Text>
+      <Image style = {styles.img} source={require('../TCC/assets/splash.png')} />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img: {
+    width: '100px',
+    height: '100px'
+  }
 });
