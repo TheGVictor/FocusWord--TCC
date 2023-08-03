c617a553a66b76adb8959d1d32af271e47bca6ad
MuriloBC2 <murilobarrosdocarmo@gmail.com>
2023-08-03 07:43:43 -0300

    teste

--- Focus/App.js
+++ Focus/App.js
@@ -18,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FF862E',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
