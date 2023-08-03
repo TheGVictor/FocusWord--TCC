8dc0ca322ca423e07dab8a2fab7711e8e5a2b339
TheGVictor <gabrielsancinetti@gmail.com>
2023-08-03 07:44:54 -0300

    Teste2

--- Focus/App.js
+++ Focus/App.js
@@ -18,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: '#FF862E',
     alignItems: 'center',
     justifyContent: 'center',
   },
