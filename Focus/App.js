7cb5a247039c05838a5617d08d69f1e90ef58812
TheGVictor <gabrielsancinetti@gmail.com>
2023-08-03 08:59:56 -0300

    Pastas organizadas

--- Focus/App.js
+++ Focus/App.js
@@ -8,9 +8,14 @@ export default function App() {
       <ActivityIndicator size='large' style = {styles.loading}/>
 
       <Image
-      style = {styles.img} 
-      source = {require('./src/images/Tela_carregamento/original_sem_fundo.png')} />
+      style = {styles.logo} 
+      source = {require('./src/images/Tela_carregamento/logoLoading.png')} />
       <StatusBar style="auto" />
+
+      <Image
+      style = {styles.logoAuth}
+      source = {require('./src/images/Tela_carregamento/logoAuth.png')}
+      />
     </View>
   );
 }
@@ -22,38 +27,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  img: {
+  logo: {
     width: 250,
     height: 250,
-    backgroundColor: '#FFF',
     borderRadius: 150
   },
-  txt: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-    color:"#000",
-    fontSize: 25,
-    marginBottom: 10,
-    marginTop: 10
-  },
-  loading: {
-    marginBottom: 10,
+  logoAuth: {
+    width: 90,
+    height: 45,
+    bottom: 10,
+    position: 'absolute'
   }
-
-
-
-//   /* Circulo_roxo 1 */
-
-// position: absolute;
-// width: 192px;
-// height: 198px;
-
-// background: #D8630E;
-// box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25);
-
-
 
   
 });
 
-const Subtitle = styled.text``
