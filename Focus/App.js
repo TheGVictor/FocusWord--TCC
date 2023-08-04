097de67e34d8fede767944622e77daa62bda6169
Gabriel Sancinetti <gabrielsancinetti@gmail.com>
2023-08-03 21:48:56 -0300

    Pastas realocas em seus lugares. App.js limpo

--- Focus/App.js
+++ Focus/App.js
@@ -1,53 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, ActivityIndicator, Animated } from 'react-native';
-import { Easing, Keyframe, RotateInDownLeft } from 'react-native-reanimated';
-import { keyframes, styled } from 'styled-components';
+import LoadingScreen from './src/screens/LoadingScreen';
 
-export default function App() {
+export default function App(){
   return (
-    <View style={styles.container}>
-
-      <Image
-      style = {styles.logo} 
-      source = {require('./src/images/Tela_carregamento/logoLoading.png')} />
-      
-    <Image
-    style = {styles.circle}
-    source={require('./src/images/Tela_carregamento/circleLoading.png')}/>
-
-      <Image
-      style = {styles.logoAuth}
-      source = {require('./src/images/Tela_carregamento/logoAuth.png')}
-      />
-
-    </View>
-  );
+    <LoadingScreen/>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FF862E',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    width: 250,
-    height: 250,
-    borderRadius: 150
-  },
-  logoAuth: {
-    width: 90,
-    height: 45,
-    bottom: 10,
-    position: 'absolute'
-  },
-
-  circle: {
-    width: 300,
-    height: 300,
-    position: 'absolute',
-  }
-
-
-});
