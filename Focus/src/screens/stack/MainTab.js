3f93417375633b2c963fa063db63e799bb01694a
Gabriel Sancinetti <gabrielsancinetti@gmail.com>
2023-08-19 10:40:58 -0300

    Tab bar estilizada. Próximos passos seriam a organização das routes e finalizar as telas restantes

--- Focus/src/screens/stack/MainTab.js
+++ Focus/src/screens/stack/MainTab.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Image } from "react-native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import RewardScreen from "../Reward"
 import PauseScreen from "../Pause"
@@ -8,15 +9,44 @@ const Tab = createBottomTabNavigator()
 
 export default function MainTab() {
     return(
-    <Tab.Navigator screenOptions={{tabBarStyle: {
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-        position: 'absolute',
-        backgroundColor: '#0FC2C0',
-    }}}>
-        <Tab.Screen name="Max" component={PauseScreen} options={{headerShown: false}} />    
-        <Tab.Screen name="Mapa" component={RewardScreen} options={{headerShown: false}}/>    
-        <Tab.Screen name="Loja" component={WordScreen} options={{headerShown: false}}/>   
+    <Tab.Navigator screenOptions={{
+        tabBarStyle: {
+            borderTopLeftRadius: 30,
+            borderTopRightRadius: 30,
+            position: 'absolute',
+            height: 60,
+            backgroundColor: '#0FC2C0'},
+            tabBarLabelStyle: {
+                fontWeight: '700',
+                fontSize: '30',
+                color: '#000'
+            }
+        
+            }}>
+        <Tab.Screen name="Max" component={PauseScreen} options={{headerShown: false, 
+         tabBarIcon: ({focused}) => {
+                if(focused) {
+                    return <Image source = {require('../../images/tabBarIcons/maxIcon.png')} style = {{width: 65, height: 65, marginBottom: 20,}}/>
+                }
+                return <Image source = {require('../../images/tabBarIcons/maxIcon.png')} style = {{width: 50, height: 50}}/>
+            
+        }}} />    
+        <Tab.Screen name="Mapa" component={RewardScreen} options={{headerShown: false,
+                 tabBarIcon: ({focused}) => {
+                    if(focused) {
+                        return <Image source = {require('../../images/tabBarIcons/mapIcon.png')} style = {{width: 65, height: 65, marginBottom: 20,}}/>
+                    }
+                    return <Image source = {require('../../images/tabBarIcons/mapIcon.png')} style = {{width: 50, height: 50}}/>
+                
+                
+                }}}/>    
+        <Tab.Screen name="Loja" component={WordScreen} options={{headerShown: false,
+            tabBarIcon: ({focused}) => {
+                if(focused) {
+                    return <Image source = {require('../../images/tabBarIcons/storeIcon.png')} style = {{width: 65, height: 65, marginBottom: 20,}}/>
+                }
+                return <Image source = {require('../../images/tabBarIcons/storeIcon.png')} style = {{width: 50, height: 50}}/>
+        }}}/>   
     </Tab.Navigator>
     )
 }
