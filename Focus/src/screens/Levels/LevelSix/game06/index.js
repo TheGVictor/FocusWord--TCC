293433737e8463f9ca6d86b62196502599bb9cac
TheGVictor <gabrielsancinetti@gmail.com>
2023-10-04 10:20:23 -0300

    Tirando o BR

--- Focus/src/screens/Levels/LevelSix/game06/index.js
+++ Focus/src/screens/Levels/LevelSix/game06/index.js
@@ -13,11 +13,7 @@ export default function WordScreenSix() {
 
             <SafeAreaView style={styles.header}>
                 <View style={styles.headerContent}>
-<<<<<<< HEAD
-                    <Text style={styles.textHeader}>06 <br/>Profissões</Text>
-=======
                     <Text style={styles.textHeader}>06 Profissões</Text>
->>>>>>> 1de0c741d4121e578ae1280411d4020aaf99943d
 
                     <Text style={styles.textHeaderWords}>
                         07/07
