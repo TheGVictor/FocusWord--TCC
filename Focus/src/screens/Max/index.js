9378d3a318af042dff03037b76895faf944f9192
TheGVictor <gabrielsancinetti@gmail.com>
2023-10-20 16:18:43 -0300

    Criando modal (tela do Max)

--- Focus/src/screens/Max/index.js
+++ Focus/src/screens/Max/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native'
+import { View, Image, Text, TouchableOpacity, Modal } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
 import styles from './style';
 
