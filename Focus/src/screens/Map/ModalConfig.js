8402270b53d436eb1e52b8a0d36970b32c0cbdfb
TheGVIctor <gabrielsancinetti@gmail.com>
2023-11-16 11:31:47 -0300

    Modal 90% completo. Basta apenas linkar com o suporte ao pais!

--- Focus/src/screens/Map/ModalConfig.js
+++ Focus/src/screens/Map/ModalConfig.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { View, Image, Text, TouchableOpacity, Modal, TouchableHighlight, Switch } from 'react-native'
+import { View, Image, Text, TouchableOpacity, Modal, TouchableHighlight, Switch, BackHandler } from 'react-native'
 import styles from './stylesModal'
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
 
 export default function ModalConfig() {
+
+  const navigation = useNavigation()
 
   const [userName, setUserName] = useState(null);
 
@@ -37,11 +40,11 @@ export default function ModalConfig() {
 
         <View style={styles.stayConect}>
 
-          <Image source={require('../../images/mapScreen/icon_profile.png')} style = {styles.iconModal}/>
+          <Image source={require('../../images/mapScreen/icon_profile.png')} style={styles.iconModal} />
 
           <Text style={styles.textModalContent}>Manter-se conectado</Text>
           <Switch
-            style = {{position: 'absolute', right: 10}}
+            style={{ position: 'absolute', right: 10 }}
             trackColor={{ false: '#767577', true: '#FF8126' }}
             thumbColor='#FFF'
             ios_backgroundColor="#3e3e3e"
@@ -50,31 +53,36 @@ export default function ModalConfig() {
 
         </View>
 
-        <View style={styles.support}>
 
-        <Image source={require('../../images/mapScreen/icon_support.png')} style = {styles.iconModal}/>
+        <TouchableOpacity>
+          <View style={styles.support}>
+            <Image source={require('../../images/mapScreen/icon_support.png')} style={styles.iconModal} />
+            <Text style={styles.textModalContent}>Suporte aos pais</Text>
+          </View>
+        </TouchableOpacity>
 
-          <Text style={styles.textModalContent}>Suporte aos pais</Text>
-        </View>
+        <TouchableOpacity onPress={() => navigation.navigate('Terms')}>
+          <View style={styles.security}>
 
-        <View style={styles.security}>
+            <Image source={require('../../images/mapScreen/icon_security.png')} style={styles.iconModal} />
 
-        <Image source={require('../../images/mapScreen/icon_security.png')} style = {styles.iconModal}/>
+            <Text style={styles.textModalContent}>Política de Segurança</Text>
+          </View>
+        </TouchableOpacity>
 
-          <Text style={styles.textModalContent}>Política de Segurança</Text>
-        </View>
+        <TouchableOpacity onPress={() => BackHandler.exitApp()}>
+          <View style={styles.closeApp}>
 
-        <View style={styles.closeApp}>
+            <Image source={require('../../images/mapScreen/icon_close.png')} style={styles.iconModal} />
 
-        <Image source={require('../../images/mapScreen/icon_close.png')} style = {styles.iconModal}/>
-
-          <Text style={styles.textModalContent}>Fechar o aplicativo</Text>
-        </View>
+            <Text style={styles.textModalContent}>Fechar o aplicativo</Text>
+          </View>
+        </TouchableOpacity>
 
       </View>
 
-      <View style = {styles.footer}>
-        <Image source={require('../../images/mapScreen/auth.png')} style = {{height: 35, width: 70}}/>
+      <View style={styles.footer}>
+        <Image source={require('../../images/mapScreen/auth.png')} style={{ height: 35, width: 70 }} />
       </View>
     </View>
 
