1d185c8cfd4e8e51a117cb685298e931e5e939c5
TheGVictor <gabrielsancinetti@gmail.com>
2023-11-17 08:35:41 -0300

    Formatação de data quase feito (corrigir bug da '/')

--- Focus/src/screens/Login/year.js
+++ Focus/src/screens/Login/year.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Image, Text, TextInput, TouchableOpacity} from 'react-native';
+import { View, Image, Text, TextInput, TouchableOpacity } from 'react-native';
 import styles from './style';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -12,39 +12,56 @@ export default function LoginYearScreen() {
   const [year, setYear] = useState('')
 
   const handleSaveYear = async () => {
-      if(year) {
-        try{
-          await AsyncStorage.setItem('year', year)
-        } catch(error) {
-          alert.error('Erro ao salvar o seu nome! Código: '+error)
-        }
-        navigation.navigate('EmailScreen')
+    if (year) {
+      try {
+        await AsyncStorage.setItem('year', year)
+      } catch (error) {
+        alert.error('Erro ao salvar o seu ano de nascimento! Código: ' + error)
       }
+      navigation.navigate('EmailScreen')
+    }
+  }
+
+  const formatInput = (date) => {
+    const numeric = date.replace(/\D/g, ' ')
+
+    if (numeric <= 2) {
+      setYear(numeric)
+
+    }
+    else if (numeric.length <= 4) {
+      setYear(`${numeric.slice(0, 2)}/${numeric.slice(2)}`);
+    }
+    else {
+      setYear(`${numeric.slice(0, 2)}/${numeric.slice(2, 4)}/${numeric.slice(4, 8)}`);
+    }
   }
 
   return (
     <SafeAreaView style={styles.container}>
 
-      <Image source = {require('../../images/startScreen/focusWord.png')} style = {styles.focusImg}/>
+      <Image source={require('../../images/startScreen/focusWord.png')} style={styles.focusImg} />
 
-      <View style = {styles.mainContent}>
-        <Text style = {styles.textInform}>Informe seu ano de nascimento</Text>
+      <View style={styles.mainContent}>
+        <Text style={styles.textInform}>Informe seu ano de nascimento</Text>
         <TextInput
-         style = {styles.inputText} 
-         placeholder='Digite seu nome'
-         onChangeText={setYear}
-         value= {year}
-         ></TextInput>
+        maxLength={10}
+          style={styles.inputText}
+          placeholder='Digite sua data de nascimento'
+          onChangeText={(text) => formatInput(text)}
+          keyboardType="numeric"
+          value={year}
+        ></TextInput>
 
-        <TouchableOpacity style = {styles.button} onPress={handleSaveYear}>
-          <Text style = {styles.textButton}>Continuar</Text>
+        <TouchableOpacity style={styles.button} onPress={handleSaveYear}>
+          <Text style={styles.textButton}>Continuar</Text>
         </TouchableOpacity>
 
       </View>
-      
-      
+
+
     </SafeAreaView>
-    
+
   );
-  
+
 }
